de38863f733e1e506bffb0b74dd36e78979f3c2d
Ruslan Sadykov <ruslan.sn@icloud.com>
2023-04-29 15:52:35 +0300

    Initial Commit

---
+++ FoundThinks/FoundThinksApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoundThinksApp.swift
+//  FoundThinks
+//
+//  Created by Руслан Садыков on 29.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct FoundThinksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
